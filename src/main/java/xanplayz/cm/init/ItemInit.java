50c4e0695948cc5ac1c2aa14677673d02bab58a8
xanplayzgamez <32940506+xanplayzgamez@users.noreply.github.com>
2017-11-23 11:08:48 -0500

    Create ItemInit.java

---
+++ src/main/java/xanplayz/cm/init/ItemInit.java
@@ -0,0 +1,15 @@
+package xanplayz.cm.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+import xanplayz.cm.objects.items.ItemBase;
+
+
+public class ItemInit {
+	
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	public static Item COPPER_INGOT = new ItemBase("ingot_copper");
+	
+}
