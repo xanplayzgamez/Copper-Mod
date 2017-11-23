099becf43c167d64bf656fddacb51810574cb69f
xanplayzgamez <32940506+xanplayzgamez@users.noreply.github.com>
2017-11-23 11:13:05 -0500

    Create References.java

---
+++ src/main/java/xanplayz/cm/util/References.java
@@ -0,0 +1,11 @@
+package xanplayz.cm.util;
+
+public class References {
+	
+	public static final String MODID = "cm";
+	public static final String VERSION = "1.0";
+	public static final String NAME = "Copper Mod";
+	public static final String CLIENT = "xanplayz.cm.proxy.ClientProxy";
+	public static final String COMMON = "xanplayz.cm.proxy.CommonProxy";
+
+}
