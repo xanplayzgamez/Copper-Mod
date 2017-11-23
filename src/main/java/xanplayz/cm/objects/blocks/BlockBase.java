0d5b2f6bf2f1181690759415727e3f00663cb2ed
xanplayzgamez <32940506+xanplayzgamez@users.noreply.github.com>
2017-11-23 11:10:28 -0500

    Create BlockBase.java

---
+++ src/main/java/xanplayz/cm/objects/blocks/BlockBase.java
@@ -0,0 +1,32 @@
+package xanplayz.cm.objects.blocks;
+
+import xanplayz.cm.net.Main;
+import xanplayz.cm.util.IHasModel;
+import xanplayz.cm.init.BlockInit;
+import xanplayz.cm.init.ItemInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class BlockBase extends Block implements IHasModel
+{
+	public BlockBase(String name, Material material) 
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		setHardness(7f);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
