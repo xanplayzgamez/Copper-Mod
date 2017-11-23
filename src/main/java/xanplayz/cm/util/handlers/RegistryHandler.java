414b2b375b20da2453b13b8638495f8df4a444cc
xanplayzgamez <32940506+xanplayzgamez@users.noreply.github.com>
2017-11-23 11:12:22 -0500

    Create RegistryHandler.java

---
+++ src/main/java/xanplayz/cm/util/handlers/RegistryHandler.java
@@ -0,0 +1,50 @@
+package xanplayz.cm.util.handlers;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.world.WorldType;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import xanplayz.cm.init.BlockInit;
+import xanplayz.cm.init.ItemInit;
+import xanplayz.cm.util.IHasModel;
+
+@EventBusSubscriber
+public class RegistryHandler 
+{
+	@SubscribeEvent
+	public static void onItemRegister(RegistryEvent.Register<Item> event)
+	{
+		event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
+	}
+	
+	@SubscribeEvent
+	public static void onBlockRegister(RegistryEvent.Register<Block> event)
+	{
+		event.getRegistry().registerAll(BlockInit.BLOCKS.toArray(new Block[0]));
+	}
+	
+	@SubscribeEvent
+	public static void onModelRegister(ModelRegistryEvent event)
+	{
+		for(Item item : ItemInit.ITEMS)
+		{
+			if(item instanceof IHasModel)
+			{
+				((IHasModel)item).registerModels();
+			}
+		}
+		
+		for(Block block : BlockInit.BLOCKS)
+		{
+			if(block instanceof IHasModel)
+			{
+				((IHasModel)block).registerModels();
+			}
+		}
+	}
+	
+}
