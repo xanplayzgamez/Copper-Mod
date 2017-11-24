c95d5b7dcb4214c36fc1060064ef5fb84ff65313
xanplayzgamez <32940506+xanplayzgamez@users.noreply.github.com>
2017-11-23 20:54:05 -0500

    Update Main.java

--- src/main/java/xanplayz/cm/net/Main.java
+++ src/main/java/xanplayz/cm/net/Main.java
@@ -7,12 +7,30 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+package xanplayz.cm.net;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import xanplayz.cm.init.BlockInit;
+import xanplayz.cm.init.ItemInit;
 import xanplayz.cm.proxy.CommonProxy;
 import xanplayz.cm.util.References;
 
 @Mod(modid = References.MODID, name = References.NAME, version = References.VERSION)
 public class Main {
 	
+	
+	public static final CreativeTabs COPPER = new CopperTab("coppertab");
 	
 	@Instance
 	public static Main instance;
